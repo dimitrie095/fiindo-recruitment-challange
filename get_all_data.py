240c4bfdb31c0698d664dd33451b080e4538e14a
Dimitrie Tayon <inf4379@hs-worms.de>
2025-12-10 10:31:50 +0100

    Funtion to fecht all data from the database implemented

---
+++ get_all_data.py
@@ -0,0 +1,20 @@
+import sqlite3
+import pandas as pd
+
+DB_PATH = "fiindo_challenge.db"   
+
+def fetch_all(table_name):
+    conn = sqlite3.connect(DB_PATH)
+    df = pd.read_sql_query(f"SELECT * FROM {table_name}", conn)
+    conn.close()
+    return df
+
+
+if __name__ == "__main__":
+    print("=== All Tickers ===")
+    tickers = fetch_all("tickers")
+    print(tickers)
+
+    print("\n=== Industry Aggregates ===")
+    industries = fetch_all("industry_aggregates")
+    print(industries)
